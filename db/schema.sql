feceb8e326c671de6d9422c8ec24f8b56f569020
lillyleiran <lilly.leiran@gmail.com>
2022-03-12 00:00:41 -0700

    add roles and employees tables

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,21 @@ CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
+
+/* job title, role id, the department that role belongs to, and the salary for that role */
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    job_title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    departments_id INTEGER NOT NULL
+);
+
+/* employee ids, first names, last names, job title, role_id, departments, salaries, and managers that the employees report to */
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    job_title VARCHAR(30) NOT NULL,
+    manager_id INTEGER,
+    roles_id INTEGER NOT NULL
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -5,6 +5,41 @@ VALUES
     ('Back-End Web Development'),
     ('Testing'),
     ('Web Design'),
-    ('Marketing'),
-    ('Sales')
+    ('Marketing')
+;
+
+INSERT INTO roles (job_title, salary, departments_id)
+VALUES
+    ('HR Manager', 110000, 1),
+    ('HR Rep', 90000, 1),
+    ('Frontend Manager', 110000, 2),
+    ('Junior Developer', 70000, 2),
+    ('Senior Developer', 90000, 2),
+    ('Backend Manager', 110000, 3),
+    ('Junior Developer', 70000, 3),
+    ('Senior Developer', 90000, 3),
+    ('Testing Manager', 110000, 4),
+    ('Tester', 65000, 4);
+    ('Design Manager', 110000, 5),
+    ('Designer', 70000, 5),
+    ('Marketing Manager', 110000, 6),
+    ('Marketing Rep', 65000, 6)
+;
+
+INSERT INTO employees (first_name, last_name, job_title, manager_id, roles_id)
+VALUES
+    ('Mary', 'Vigos', 'HR Manager', NULL, 1),
+    ('Cherry', 'Loveless', 'HR Rep', 1, 1),
+    ('Russel', 'Phillips', 'Frontend Manager', NULL, 3),
+    ('Lilly', 'Leiran', 'Junior Developer', 3, 4),
+    ('Hope', 'Saville', 'Senior Developer', 3, 5),
+    ('Matt', 'Peterson', 'Backend Manager', NULL, 6),
+    ('Aaron', 'Senior', 'Junior Developer', 6, 7),
+    ('Steve', 'Kerkman', 'Senior Developer', 6, 8),
+    ('Travis', 'Grant', 'Testing Manager', NULL, 9),
+    ('Chrissy', 'Meier', 'Tester', 9, 10),
+    ('Kim', 'Hinson', 'Design Manager', NULL, 11),
+    ('Denise', 'Sherer', 'Designer', 11, 12),
+    ('Chelsea', 'Calhoun', 'Marketing Manager', NULL, 13),
+    ('Cortney', 'Fronk', 'Marketing Rep', 13, 14)
 ;
