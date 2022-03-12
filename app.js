8fb755c503cd8202669f2c7a11e7f98ae5ef7dcb
lillyleiran <lilly.leiran@gmail.com>
2022-03-11 21:52:15 -0700

    objects

---
+++ app.js
@@ -0,0 +1,9 @@
+const require = require('inquirer');
+const consoleTable = require('console.table');
+const mysql2 = require('mysql2');
+
+const Department = require('./lib/Department');
+const Role = require('./lib/Role');
+const Employee = require('./lib/Employee');
+
+
