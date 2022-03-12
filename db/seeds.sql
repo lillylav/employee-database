0a2863c22ea0e448c21f53d8adda1789428441b7
lillyleiran <lilly.leiran@gmail.com>
2022-03-11 22:59:12 -0700

    added departments to database

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO departments (name)
+VALUES
+    ('HR'),
+    ('Front-End Web Development'),
+    ('Back-End Web Development'),
+    ('Testing'),
+    ('Web Design'),
+    ('Marketing'),
+    ('Sales')
+;
