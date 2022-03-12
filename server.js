5e24692d7976199cf8479e19d5e3de7545420a53
lillyleiran <lilly.leiran@gmail.com>
2022-03-12 00:29:44 -0700

    display, delete, and create departments in database

--- server.js
+++ server.js
@@ -19,8 +19,29 @@ const db = mysql.createConnection(
     console.log('Connected to the company database.')
 );
 
-db.query(`SELECT * FROM departments`, (err, rows) => {
-    console.log(rows);
+// // command to display all departments
+// db.query(`SELECT * FROM departments`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// // Delete a department
+// db.query(`DELETE FROM departments WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// // Create a department
+// const sql = `INSERT INTO departments (id, name) 
+//               VALUES (?,?)`;
+// const params = [1, 'HR'];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
 });
 
 // Default response for any other request (Not Found)
