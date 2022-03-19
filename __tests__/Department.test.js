d1ec7164fe8fd034ac52b65855e9430b21a7b20d
lillyleiran <lilly.leiran@gmail.com>
2022-03-19 15:02:52 -0600

    remove all unnecessary server setup files

--- __tests__/Department.test.js
+++
@@ -1,9 +0,0 @@
-const Department = require('../lib/Department');
-
-jest.mock();
-
-test('creates department object', () => {
-    const department = new Department('HR');
-
-    expect(department.name).toBe('HR');
-});
